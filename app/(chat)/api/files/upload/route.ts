44885fb64eb679aea1fae70fafa4565cb5373338
Akshay Channabasaiah <aksc.champ@gmail.com>
2025-04-27 20:25:43 -0400

    Added word document to pdf conversion.

--- app/(chat)/api/files/upload/route.ts
+++ app/(chat)/api/files/upload/route.ts
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server';
 import { z } from 'zod';
 
 import { auth } from '@/app/(auth)/auth';
+import ILovePDFApi from '@ilovepdf/ilovepdf-nodejs';
+import ILovePDFFile from '@ilovepdf/ilovepdf-nodejs/ILovePDFFile';
 
 // Use Blob instead of File since File is not available in Node.js environment
 const FileSchema = z.object({
@@ -20,14 +22,34 @@ const FileSchema = z.object({
           'video/mp4',
           'audio/mpeg',
           'audio/mp4',
+          'image/gif',
+          'image/tiff',
+          'image/bmp',
+          'image/webp',
+          'application/msword',
           'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
           'application/pdf',
         ].includes(file.type),
       {
-        message: 'File type should be DOCX, PDF, JPEG, PNG, MP3 or MP4',
+        message:
+          'File type should be DOC, DOCX, PDF, JPEG, TIFF, PNG, GIF, BMP, WEBP, MP3 or MP4',
       },
     ),
 });
+
+const ILovePdfInstance = new ILovePDFApi(
+  process.env.ILOVEPDF_PUBLIC_KEY as string,
+  process.env.ILOVEPDF_SECRET_KEY as string,
+);
+
+export async function ConverttoPDF(fileUrl: string) {
+  const task = ILovePdfInstance.newTask('officepdf');
+  await task.start();
+  await task.addFile(fileUrl);
+  await task.process();
+  const fileData = await task.download();
+  return fileData;
+}
 
 export async function POST(request: Request) {
   const session = await auth();
@@ -67,6 +89,38 @@ export async function POST(request: Request) {
         access: 'public',
       });
 
+      if (
+        file.type ===
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document' ||
+        file.type === 'application/msword'
+      ) {
+        try {
+          // Convert WORD to PDF
+          const pdfBuffer = await ConverttoPDF(data.url);
+          const pdfFileName = `${filename.substring(0, filename.lastIndexOf('.')) || filename}.pdf`;
+          try {
+            const data = await put(
+              `${pdfFileName}`,
+              pdfBuffer.buffer as ArrayBuffer,
+              {
+                access: 'public',
+              },
+            );
+
+            return NextResponse.json(data);
+          } catch (error) {
+            return NextResponse.json(
+              { error: 'Upload failed after conversion to PDF' },
+              { status: 500 },
+            );
+          }
+        } catch (error) {
+          return NextResponse.json(
+            { error: 'Convert from Word to PDF failed' },
+            { status: 500 },
+          );
+        }
+      }
       return NextResponse.json(data);
     } catch (error) {
       return NextResponse.json({ error: 'Upload failed' }, { status: 500 });
